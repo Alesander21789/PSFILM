2ad628cea4c09abbf948cec5d15e09b238429549
Alesander <sanderfene21789@gmail.com>
2018-11-09 15:20:51 +0100

    Initial commit

---
+++ app/src/main/java/com/cifprodolfoucha/PSFilms/Activity_Informacion.java
@@ -0,0 +1,13 @@
+package com.cifprodolfoucha.PSFilms;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_Informacion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_informacion_activity);
+    }
+}
---
+++ app/src/main/java/com/cifprodolfoucha/PSFilms/Activity_Login.java
@@ -0,0 +1,13 @@
+package com.cifprodolfoucha.PSFilms;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Activity_Login extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_login_activity);
+    }
+}
