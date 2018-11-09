9032b6d53d6c647d9936fea6d1b54f5a156ed705
Alesander <sanderfene21789@gmail.com>
2018-11-09 15:29:36 +0100

    clases

---
+++ app/src/main/java/com/cifprodolfoucha/PSFilms/clases/Actor.java
@@ -0,0 +1,71 @@
+package com.cifprodolfoucha.PSFilms.clases;
+
+public class Actor {
+
+    private String nombre;
+    private String artista;
+    private int imagen;
+
+    public Actor(){
+
+    }
+
+    public Actor(String nombre, String artista, int imagen) {
+        this.nombre = nombre;
+        this.artista = artista;
+        this.imagen = imagen;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getArtista() {
+        return artista;
+    }
+
+    public void setArtista(String artista) {
+        this.artista = artista;
+    }
+
+    public int getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(int imagen) {
+        this.imagen = imagen;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
