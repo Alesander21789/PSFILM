6404be670861a4085027bf3efcc5a64e53a24fe8
Alesander <sanderfene21789@gmail.com>
2018-11-13 17:25:12 +0100

    adapters

--- app/src/main/java/com/cifprodolfoucha/PSFilms/sacarFoto.java
+++ app/src/main/java/com/cifprodolfoucha/PSFilms/sacarFoto.java
@@ -4,22 +4,27 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.CancellationSignal;
+import android.provider.MediaStore;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.SearchView;
 import android.widget.Toast;
+
+import com.cifprodolfoucha.PSFilms.clases.Save;
 
 public class sacarFoto extends Activity implements View.OnClickListener {
     // Usado por si necesitamos diferentes permisos, para identificar cual de ellos es
     private final int CODIGO_IDENTIFICADOR=1;
-
+    ImageView image;
     private void guardarFoto(){
 
     }
@@ -28,6 +33,7 @@ public class sacarFoto extends Activity implements View.OnClickListener {
     //...
     private static final int CAMERA_PERMISSION_REQUEST_CODE = 1;
     private static final int SENSOR_PERMISSION_REQUEST_CODE = 2;
+    private static final  int REQUEST_IMAGE_CAPTURE=1;
     //...
 
     @RequiresApi(api = Build.VERSION_CODES.M)
@@ -66,8 +72,52 @@ public class sacarFoto extends Activity implements View.OnClickListener {
         }
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+
+        if (requestCode==REQUEST_IMAGE_CAPTURE && resultCode==RESULT_OK) {
+
+
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+
+          image = findViewById(R.id.imImagen_sacarFoto);
+          image.setImageBitmap(imageBitmap);
+
+
+          image.setOnLongClickListener(new View.OnLongClickListener() {
+              @Override
+              public boolean onLongClick(View v) {
+                  //convertir imagen a bitmap
+                  image.buildDrawingCache();
+                  Bitmap bmap = image.getDrawingCache();
+
+                  //guardar imagen
+                  Save savefile = new Save();
+                  savefile.SaveImage(sacarFoto.this, bmap);
+                  return false;
+              }
+          });
+        }
+    }
+
+
+
+
+
     private void permissionGranted() {
         //Toast.makeText(this, getString(R.string.permission_granted), Toast.LENGTH_SHORT).show();
+
+
+
+        Intent tomarFoto= new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        if (tomarFoto.resolveActivity(getPackageManager()) !=null) {
+
+            startActivityForResult(tomarFoto, REQUEST_IMAGE_CAPTURE);
+
+        }
 
 
         Toast.makeText(this, "Aceptado", Toast.LENGTH_SHORT).show();
@@ -111,6 +161,7 @@ public class sacarFoto extends Activity implements View.OnClickListener {
 
 
 
+
         Button boton = (Button)findViewById(R.id.btnCameraPermission);
         boton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -130,6 +181,7 @@ public class sacarFoto extends Activity implements View.OnClickListener {
         });
 
     }
+
 
   @Override
   public void onRequestPermissionsResult(int requestCode, String[] permissions,  int[] grantResults) {
