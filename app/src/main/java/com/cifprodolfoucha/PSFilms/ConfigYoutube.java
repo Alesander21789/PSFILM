6d7b693aefd22b0d96a38b60385fae18b34236ea
Alesander <sanderfene21789@gmail.com>
2018-11-15 20:28:11 +0100

    youtube

---
+++ app/src/main/java/com/cifprodolfoucha/PSFilms/ConfigYoutube.java
@@ -0,0 +1,9 @@
+package com.cifprodolfoucha.PSFilms;
+
+public class ConfigYoutube {
+    // Google Console APIs developer clave
+    public static final String
+            DEVELOPER_KEY = "AIzaSyB9O7is4GquGCS1mecxa1VI3s_c2QoXq7I";
+    // YouTube video id
+    public static final String YOUTUBE_VIDEO_CODE = "j633tS054VY";
+}
